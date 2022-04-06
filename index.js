a07c1bd98699b648c9c4ed0b97771165daf5e4c1
Alexander Schuessler <schuessler.alex@gmail.com>
2022-04-06 09:47:10 -0500

    commit tree 576884bc71e24b8886b0142c6a9529dcb227ffda

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+let destructivelyAppendCat = (name) => {
+   let newCats =cats.push(name)
+   return newCats
+}
+// console.log(destructivelyAppendCat('Ralph'));
+let destructivelyPrependCat = (name) => {
+    let newCats2 =cats.unshift(name)
+    return newCats2
+ }
